19f538d25ed98b0d6fedab3aea8cea838e41e42e
Fluke988 <jagtaphrushi@gmail.com>
2022-02-06 13:44:33 +0530

    Commit4
    Tried adding navmesh agent to one enemy object and instantiating

--- HypercasualGame1/Assets/Scripts/EnemyAI.cs
+++ HypercasualGame1/Assets/Scripts/EnemyAI.cs
@@ -3,22 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class EnemySpawner : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     public GameObject theEnemy;
     private int xPos, xPos1;
     private int zPos, zPos1;
     public int enemyCount;
- 
+
     public GameObject targetPlayer;
     NavMeshAgent enemyAgent;
 
-    private void Start()
+    void Start()
     {
         enemyAgent = GetComponent<NavMeshAgent>();
         StartCoroutine(EnemyDrop());
     }
-
     IEnumerator EnemyDrop()
     {
         while (enemyCount < 5)
@@ -27,21 +26,14 @@ public class EnemySpawner : MonoBehaviour
             zPos = Random.Range(-5, -10);
             Instantiate(theEnemy, new Vector3(xPos, 1, zPos), Quaternion.identity);
 
-            yield return new WaitForSeconds(1f);
-
-            xPos1 = Random.Range(5, 10);
-            zPos1 = Random.Range(5, 10);
-            Instantiate(theEnemy, new Vector3(xPos1, 1, zPos1), Quaternion.identity);
-
+            enemyAgent.SetDestination(targetPlayer.transform.position);
             yield return new WaitForSeconds(1f);
             enemyCount += 1;
         }
     }
-
-    void Update()
-    {
-        enemyAgent.SetDestination(targetPlayer.transform.position);
-    }
-
+    //void Update()
+    //{
+    //    enemyAgent.SetDestination(targetPlayer.transform.position);
+        
+    //}
 }
-    
